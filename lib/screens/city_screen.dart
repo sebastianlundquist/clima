55b1cf440ef46261391c80534bd83d624a63b892
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-09-09 20:37:52 +0200

    Fix keyboard squishing background image

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -12,6 +12,7 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
